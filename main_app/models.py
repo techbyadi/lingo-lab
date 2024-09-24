dd7226e314be2d190af8e5925ab0a434abbd1472
techbyadi <pssharma.aditya@gmail.com>
2024-09-24 07:47:06 -0700

    fetch words from DB

--- main_app/models.py
+++ main_app/models.py
@@ -1,14 +1,9 @@
 from django.db import models
-from django.urls import reverse
-from datetime import date
-from django.contrib.auth.models import User
 
 class Word(models.Model):
     word = models.CharField(max_length=20)
     origin = models.CharField(max_length=50)
     usage_in_sentence = models.TextField()
-    ##created_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    ##created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.word
--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,25 @@
 from django.shortcuts import render
-
-# Add the following import
 from django.http import HttpResponse
+from .models import Word
 
-class Word:  
+class Word_old:  
   def __init__(self, word, origin, usage_in_sentence):
     self.word = word
     self.origin = origin
     self.usage_in_sentence = usage_in_sentence
 
-words = [
+words_old = [
   Word('First', 'Greek', 'It\'s first word'),
   Word('Second', 'Greek', 'Looks like a second word.'),
 ]
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello Word!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def word_index(request):
-  return render(request, 'words/index.html', { 'words': words })
+  words = Word.objects.all()
+  return render(request, 'words/index.html', {'words': words})
