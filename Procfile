47d9caf2637049a677745c367c4abcbeddec058f
techbyadi <pssharma.aditya@gmail.com>
2024-09-25 08:32:26 -0700

    deployment setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn lingolab.wsgi
--- lingolab/settings.py
+++ lingolab/settings.py
@@ -14,6 +14,7 @@ import os
 env = environ.Env()
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,9 +27,11 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = 'django-insecure-$h&6b31baerj45+h*$w8y^o-t=ju=(e5))u&=bwwz^-c0c=_vs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if not 'ON_HEROKU' in os.environ:
+    DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -41,10 +44,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -77,15 +82,26 @@ WSGI_APPLICATION = 'lingolab.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('NEON_DATABASE_NAME'),
-        'USER': env('NEON_USER'),
-        'PASSWORD': env('NEON_PASSWORD'),
-        'HOST': env('NEON_HOST'),
-        'PORT': '5432',
-        'OPTIONS': {'sslmode': 'require'}
+
+if 'ON_HEROKU' in os.environ:
+    DATABASES = {
+        "default": dj_database_url.config(
+            env='DATABASE_URL',
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        ),
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': env('NEON_DATABASE_NAME'),
+            'USER': env('NEON_USER'),
+            'PASSWORD': env('NEON_PASSWORD'),
+            'HOST': env('NEON_HOST'),
+            'PORT': '5432',
+            'OPTIONS': {'sslmode': 'require'}
     }
 }
 
@@ -125,6 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = 'word-index'
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'home'
